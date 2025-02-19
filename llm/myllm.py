cb109c1d3713887276db6de43a7ac48e5dd35d50
huchen <huchen@lilith.com>
2025-02-19 16:11:56 +0800

    finish 1st version, next step pretraining and test with GPT2model

--- llm/myllm.py
+++ llm/myllm.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,
@@ -12,13 +13,13 @@ GPT_CONFIG_124M = {
 }
 
 
-class DummyGPTModel(nn.Module):
+class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
-        self.trf_blocks = nn.Sequential(*[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
         self.final_norm = LayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
     
@@ -33,13 +34,6 @@ class DummyGPTModel(nn.Module):
         logits = self.out_head(x)
         return logits
     
-class DummyTransformerBlock(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-    
-    def forward(self, x):
-        return x
-
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
@@ -52,4 +46,93 @@ class LayerNorm(nn.Module):
         var = x.var(dim=-1, keepdim=True, unbiased=False)
         x_norm = (x - mean) / torch.sqrt(var + eps)
         return x * self.scale + self.shift
+        
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * x**3)))
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.linear1 = nn.Linear(cfg["emb_dim"], 4*cfg["emb_dim"])
+        self.linear2 = nn.Linear(4*cfg["emb_dim"], cfg["emb_dim"])
+        self.gelu = GELU()
+    
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.gelu(x)
+        x = self.linear2(x)
+        return x
+        
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, n_heads,qkv_bias=False):
+        super().__init__()
+        assert(d_out % n_heads == 0)
+        self.d_out = d_out
+        self.num_heads = n_heads
+        self.head_dim = d_out // n_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.outproj = nn.Linear(d_out, d_out)
+        self.register_buffer("mask", torch.triu(torch.full((context_length, context_length), True, dtype=torch.bool), diagonal=1))
+
+    def forward(self, x):
+        batch_size, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        
+        # (batch_size, n_heads, num_tokens, head_dim)
+        keys = keys.transpose(1,2) 
+        queries = queries.transpose(1,2)
+        values = values.transpose(1,2)
+
+        attn_scores = queries @ keys.transpose(2,3)
+        mask_bool = self.mask[:num_tokens, :num_tokens]
+
+        # Causal mask
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores/attn_scores.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # -> (batch_size, num_tokens, n_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1,2)
+        context_vec = context_vec.contiguous().view(batch_size, num_tokens, self.d_out)
+        context_vec = self.outproj(context_vec)
+        return context_vec
+    
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.attn = MultiHeadAttention(cfg["emb_dim"], cfg["emb_dim"], cfg["context_length"], cfg["drop_rate"], cfg["n_heads"])
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop = nn.Dropout(cfg["drop_rate"])
+        self.ffn = FeedForward(cfg)
+
+    def forward(self,x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.attn(x)
+        x = self.drop(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ffn(x)
+        x = self.drop(x)
+        x = x + shortcut
+        return x
+       
         
